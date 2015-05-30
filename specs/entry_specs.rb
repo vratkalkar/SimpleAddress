285db6ceb32f45ca24e776bdd709329c75c7fa20
Vivek Ratkalkar <vratkalkar@live.com>
2015-05-30 12:58:51 -0400

    add address book model

--- specs/entry_specs.rb
+++ specs/entry_specs.rb
@@ -3,18 +3,26 @@ RSpec.describe Entry do
   context "attributes" do
 
     it "should respond to name" do
-      entry = Entry.new
+      entry = Entry.new("Bo Jackson", "123-456-890", "great@sports.com")
       expect(entry).to respond_to(:name)
     end
 
     it "should respond to phone number" do
-      entry= Entry.new
+      entry = Entry.new("Bo Jackson", "123-456-890", "great@sports.com")
       expect(entry).to respond_to(:phone_number)
     end
 
     it "should respond to email" do
-      entry = Entry.new
+      entry =  Entry.new("Bo Jackson", "123-456-890", "great@sports.com")
       expect(entry).to respond_to(:email)
+    end
+  end
+
+  context ".to_s" do
+    it "prints an entry as a string" do
+      entry =  Entry.new("Bo Jackson", "123-456-890", "great@sports.com")
+      expected_string =  "Name: Bo Jackson\nPhone Number: 123-456-890\nEmail: great@sports.com"
+      expect(entry.to_s).to eq(expected_string)
     end
   end
 end
