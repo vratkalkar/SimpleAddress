a762553cc2e9b010267c218695938bf5992ed750
Vivek Ratkalkar <vratkalkar@live.com>
2015-05-31 16:18:34 -0400

    add and implement csv parsing

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
-require_relative "entry.rb"
+require_relative "entry"
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -16,6 +17,17 @@ class AddressBook
         index += 1
      end
     @entries.insert(index, Entry.new(name, phone, email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+    return csv.count
   end
 
   def remove_entry(name, phone, email)
--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -1,31 +1,33 @@
 RSpec.describe AddressBook do
+  let(:book) {AddressBook.new}
+
+  def check_entry(entry,expected_name, expected_number, expected_email)
+    expect(entry.name).to eql expected_name
+    expect(entry.phone_number).to eql expected_number
+    expect(entry.email).to eql expected_email
+  end
 
  context "attributes" do
   it "should respond to entries" do
-    book = AddressBook.new
     expect(book).to respond_to(:entries)
   end
 
   it "should initialize entries as an array" do
-    book = AddressBook.new
     expect(book.entries).eql? "[]"
   end
 
   it "should initialize entries as empty" do
-    book = AddressBook.new
     expect(book.entries).eql? 0
   end
 
  context ".add_entry" do
   it "adds only one entry to the address book" do
-    book = AddressBook.new
     book.add_entry("Bo Jackson", "123-456-890", "great@sports.com")
 
     expect(book.entries).eql? 1
  end
 
   it "adds the correct information to entries" do
-    book = AddressBook.new
     book.add_entry("Bo Jackson", "123-456-890", "great@sports.com")
     new_entry = book.entries[0]
 
@@ -38,10 +40,23 @@ RSpec.describe AddressBook do
 
   context ".remove_entry" do
     it "removes only one entry from the address book" do
-      book = AddressBook.new
       book.remove_entry("Bo Jackson", "123-456-890", "great@sports.com")
 
       expect(book.entries).eql? 0
     end
   end
+
+  describe ".import_from_csv" do
+    it "imports the correct number of entries" do
+    book.import_from_csv("entries.csv")
+    book_size = book.entries.size
+    expect(book_size).to eql 1
+  end
+  it "checks the details of the first entry" do
+    book.import_from_csv("entries.csv")
+    entry_one = book.entries[0]
+    check_entry(entry_one, "Vivek", "123-456-1234", "vratkalkar@email.com")
+  end
+ end
+
 end
